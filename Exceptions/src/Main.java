517bf1352921c447440ebbb3befed0bf17192da0
Kiselevnp <kiselev_nikolay@inbox.ru>
2024-08-30 09:17:54 +0300

    аfixed

--- Exceptions/src/Main.java
+++ Exceptions/src/Main.java
@@ -4,34 +4,42 @@ public class Main {
                 {"1", "2", "3", "4"},
                 {"5", "6", "7", "8"},
                 {"9", "10", "11", "12"},
-                {"13", "14", "15", "16"},
-                      };
+                {"13", "14", "15", "16"}
+        };
 
         try {
             int result = sumArrayElements(array);
             System.out.println("Сумма элементов массива: " + result);
         } catch (MyArraySizeException e) {
-            System.out.println("Ошибка размера массива: " + e.getMessage());
+            System.out.println("Неправильный размер массива: " + e.getMessage());
         } catch (MyArrayDataException e) {
-            System.out.println("Ошибка данных в массиве: " + e.getMessage());
+            System.out.println("Ошибка в данных массива: " + e.getMessage());
         }
     }
 
     public static int sumArrayElements(String[][] array) throws MyArraySizeException, MyArrayDataException {
-        if (array.length != 4 || array[0].length != 4) {
-            throw new MyArraySizeException("Массив должен быть размером 4x4.");
+        if (array.length != 4) {
+            throw new MyArraySizeException("Нужен массив размером 4x4.");
+        }
+
+        for (int i = 0; i < array.length; i++) {
+            if (array[i].length != 4) {
+                throw new MyArraySizeException("Нужен массив размером 4x4.");
+            }
         }
 
         int sum = 0;
+
         for (int i = 0; i < array.length; i++) {
             for (int j = 0; j < array[i].length; j++) {
                 try {
                     sum += Integer.parseInt(array[i][j]);
                 } catch (NumberFormatException e) {
-                    throw new MyArrayDataException("Ошибка в ячейке: [" + i + "][" + j + "]. Невозможно преобразовать в число: " + array[i][j]);
+                    throw new MyArrayDataException("Ошибка в ячейке: [" + i + "][" + j + "] - " + array[i][j]);
                 }
             }
         }
+
         return sum;
     }
 }
